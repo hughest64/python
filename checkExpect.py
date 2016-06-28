8eca57707afc3e46873f3dae107e51b8ded6ec6a
hughest64 <hughest64@gmail.com>
2016-06-28 16:38:55 -0500

    Update checkExpect.py

--- checkExpect.py
+++ checkExpect.py
@@ -1,11 +1,14 @@
 def checkExpect(func, ans):
-''' a function for testing other functions
+    '''
+    a function for testing other functions
     func: function with given arguments 
     ans: the expected result of evaluating func
-    returns a  string '''
+    returns a  string 
+    '''
     if func != ans:
         #show the actual result of func and what was expected
-        print 'The value, ' + str(func) + ' differs from the expected value, ' + str(ans)
+        print 'The value, ' + str(func), 
+        print ' differs from the expected value, ' + str(ans)
     else:
         # the evaluation of func matches the expected answer
         print 'The test passed!'
