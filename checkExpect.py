43375d4f65449f7b010d4e918e7bc3f5ef8aad1f
hughest64 <hughest64@gmail.com>
2016-06-28 12:08:23 -0500

    Update checkExpect.py
    updated dot string

--- checkExpect.py
+++ checkExpect.py
@@ -1,9 +1,11 @@
 def checkExpect(func, ans):
 ''' a function for testing other functions
-    takes in a function with given arguments and an expected answer.
-    evaluates true if the func evaluation matches the expected answer'''
+    func: function with given arguments 
+    ans: the expected result of evaluating func
+    returns a  string '''
     if func != ans:
         #show the actual result of func and what was expected
         print 'The value, ' + str(func) + ' differs from the expected value, ' + str(ans)
     else:
+        # the evaluation of func matches the expected answer
         print 'The test passed!'
