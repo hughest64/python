eee60fb079d924c626fb174cf67abf42ef4160a1
hughest64 <hughest64@gmail.com>
2016-07-07 13:38:52 -0500

    Update ispal.py

--- ispal.py
+++ ispal.py
@@ -6,3 +6,27 @@ def isPalindrome(aString):
         return True
     else:
        return aString[0] == aString[-1] and isPalindrome(aString[1: -1])
+       
+       
+       
+       
+def isPalindrome(aString):
+    '''
+    converts and string to just lower case letters.
+    '''
+    def makeLower(aString):
+        aString = aString.lower()
+        ans =''
+        alpha = 'abcdefghijklmnopqrstuvwxyz'
+        for letter in aString:
+            if letter in alpha:
+                ans += letter
+        return ans
+        
+    def isPal(aString):    
+        if len(aString) <= 1:
+            return True
+        else:
+            return aString[0] == aString[-1] and isPal(aString[1: -1])
+            
+    return isPal(makeLower(aString))
