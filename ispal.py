2f24dddcb36218ca782b58ba1e0a76e83c0311ed
hughest64 <hughest64@gmail.com>
2016-07-07 13:26:26 -0500

    Update ispal.py

--- ispal.py
+++ ispal.py
@@ -5,7 +5,4 @@ def isPalindrome(aString):
     if len(aString) <= 1:
         return True
     else:
-        if aString[0] == aString[-1]:
-            return isPalindrome(aString[1: -1])
-        else:
-            return False
+       return aString[0] == aString[-1] and isPalindrome(aString[1: -1])
