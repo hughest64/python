6bdda9ced25d734ac9276caf244eeb270cb9e0f0
hughest64 <hughest64@gmail.com>
2016-07-08 15:07:42 -0500

    Update satifies_L.py

--- satifies_L.py
+++ satifies_L.py
@@ -1,8 +1,5 @@
 def satisfiesF(L):    
-    L2 = []
-    for x in L:
-        if f(x) == True:
-            L2.append(x)
-        
-    L = L2
+    for s in L:
+        if f(s) == False:
+            L.remove(s)
     return len(L)
